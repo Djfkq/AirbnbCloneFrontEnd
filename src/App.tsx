4fe6a0b07a174a57557e7292313e3934bfb2a22c
KSH <kangsenghun@hanmail.net>
2024-01-19 00:26:31 +0900

    17.2 Setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
-import React from 'react';
+import React from "react";
+import { Text } from "@chakra-ui/react";
 
 function App() {
-  return <div />
-
+  return (
+    <div>
+      <Text fontSize={"6xl"} color={"red.200"}>
+        It Works!!!
+      </Text>
+    </div>
+  );
 }
 
 export default App;
